108843bec99ef5e5f90b9f789409592147e2e180
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2023-08-21 00:35:49 +0530

    initial creation

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Threads Clone App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) { 
+
+  return <ClerkProvider>
+
+  </ClerkProvider>  
+  ;
+}   
